adf62dd613607ac19d62aea0d523378477573487
Joshy Orndorff <admin@joshorndorff.com>
2018-11-29 20:51:06 -0500

    Basic working version.

---
+++ README.md
@@ -0,0 +1,25 @@
+freshr
+===========
+
+Start a fresh RNode with casper
+
+Usage
+-----------
+`freshr` That's it.
+
+Now you've got a casper node to develop your dapps against.
+
+Installation
+--------------
+
+Install from npm repository with `npm install --global freshr` or clone this repository.
+
+Dependencies
+---------------
+* nodejs
+* RChain [RNode](https://github.com/rchain/rchain/releases)
+
+Author and License
+------------------
+By Joshy Orndorff
+Apache-2.0 License
---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+const { execSync } = require('child_process');
+const { copyFileSync, mkdtempSync, mkdirSync } = require('fs');
+const { sep } = require('path');
+const { tmpdir } = require('os');
+
+//TODO docopt?
+
+// Create a temporary data directory
+let datadir = mkdtempSync(`${tmpdir()}${sep}freshr-`);
+
+console.log("the tempdir is " + datadir);
+
+// Copy default RNode directory to appropriate place
+//TODO Consider generating keys dynamically instead of committing
+copyFileSync(`${__dirname}${sep}rnode.toml`, `${datadir}${sep}rnode.toml`);
+mkdirSync(`${datadir}${sep}genesis`);
+copyFileSync(`${__dirname}${sep}bonds.txt` , `${datadir}${sep}genesis${sep}bonds.txt`);
+//TODO relative paths in rnode.toml are relative to the pwd, not the toml file, so
+// I'm forced to put the bonds file in the default location. Better solution is to
+// generate the toml file on the fly which would lend nicely to generating keys
+// on the fly, but would also require generating the bonds file on the fly
+
+
+// Start node
+//TODO move --has-faucet to rnode.toml
+// The line `has-faucet = true` doesn't seem to work under [server] or [validators]
+execSync(`rnode run --has-faucet --data-dir ${datadir}`, { stdio: 'inherit' });
